d74ede09c091d208cfd40b1b1574c4ef31474692
oScarFernandes <2024_oscar.fernandes@iticbcn.cat>
2024-11-19 08:38:53 +0100

    Exercicis Finalitzats

---
+++ Comparacio.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 20
+
+if num1 > num2:
+    print("El primer número és més gran")
+elif num1 < num2:
+    print("El segon número és més gran")
+else:
+    print("Els dos número són iguals")
