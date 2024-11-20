12cd054365337dfbe3e6c432f6a08db131bc36c7
oScarFernandes <2024_oscar.fernandes@iticbcn.cat>
2024-11-20 10:59:07 +0100

    .

--- Activitat 2/Comparacio.py
+++
@@ -1,9 +0,0 @@
-num1 = 10
-num2 = 20
-
-if num1 > num2:
-    print("El primer número és més gran")
-elif num1 < num2:
-    print("El segon número és més gran")
-else:
-    print("Els dos número són iguals")
